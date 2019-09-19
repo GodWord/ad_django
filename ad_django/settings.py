72fb19ecc0b5bc455888dbed3373b65fd66f87b5
‘ace’ <wangbowei163@163.com>
2019-09-19 14:01:59 +0800

    fix-增加日志文件夹创建

--- ad_django/settings.py
+++ ad_django/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ad',
         'USER': 'root',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'a.123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
