66c3065fef62bc07016a095e93567d175646d18d
vFeng <z1914007838@outlook.com>
2019-09-19 12:35:22 +0800

    fix-修复超级用户创建失败bug

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -1,8 +1,7 @@
 # -*- coding:utf-8 -*-
 from datetime import datetime
 
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager as AbstractBaseUserManager
 from django.db import models
 
 __author__ = 'zhoujifeng'
@@ -11,27 +10,61 @@ __file_name__ = 'user'
 __project_name__ = 'ad_django'
 
 
+class BaseUserManager(AbstractBaseUserManager):
+    def create_user(self, username, password=None, email=None, is_active=True, is_superuser=False):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        user = self.model(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, user, password):
+        """
+        Creates and saves a staff user with the given email and password.
+        """
+        user = self.create_user(
+            user,
+            password=password,
+        )
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password, email):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            username,
+            password=password,
+            email=email,
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser):
-    """
-    Users within the Django authentication system are represented by this
-    model.
-媒体
-    Username, password and email are required. Other fields are optional.
-    """
     GENDER_CHOICES = (
         (u'0', u'男'),
         (u'1', u'女'),
     )
     username = models.CharField(max_length=40, unique=True)
-    USERNAME_FIELD = 'username'
     image = models.ImageField(upload_to='media/images/user/', default='media/images/default.png', verbose_name='头像')
     gender = models.PositiveSmallIntegerField(default=0, choices=GENDER_CHOICES, verbose_name='性别')
-    email = models.CharField(max_length=64, default='', verbose_name='邮箱')
+    email = models.CharField(max_length=64, null=True, verbose_name='邮箱')
     is_superuser = models.BooleanField(default=False, verbose_name='是否为管理账号')
     create_time = models.DateTimeField(blank=True, null=True, default=datetime.now, verbose_name="创建时间")
     update_time = models.DateTimeField(blank=True, null=True, default=datetime.now, verbose_name="更新时间")
 
-    objects = UserManager()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = BaseUserManager()
 
     class Meta(AbstractBaseUser.Meta):
         verbose_name = '用户信息'
